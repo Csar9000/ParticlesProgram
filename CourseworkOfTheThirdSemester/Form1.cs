f999c6729024b5d4a7f0036e1550fb9a4f74d813
Csar9000 <72266005+Csar9000@users.noreply.github.com>
2020-12-18 20:02:17 +0800

    Second commit

--- CourseworkOfTheThirdSemester/Form1.cs
+++ CourseworkOfTheThirdSemester/Form1.cs
@@ -34,6 +34,7 @@ namespace CourseworkOfTheThirdSemester
         private void pictureBox1_Click(object sender, EventArgs e)
         {
             Console.WriteLine("Hello world");
+            Console.WriteLine("Hello world");
         }
 
         private void pictureBox1_Tick(object sender, EventArgs e)
